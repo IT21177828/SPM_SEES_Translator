c76303a28ad753c42ef367dd9124946842d5dce1
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-09-07 02:03:21 +0530

    Add Feedback Model

--- backend/Routers/FeedbackRouter.js
+++ backend/Routers/FeedbackRouter.js
@@ -10,18 +10,18 @@ import {
 const feedbackRouter = express.Router();
 
 // Create new feedback
-FeedbackRouter.post('/', createFeedback);
+feedbackRouter.post('/', createFeedback);
 
 // Get all feedback
-FeedbackRouter.get('/', getAllFeedback);
+feedbackRouter.get('/', getAllFeedback);
 
 // Get feedback by ID
-FeedbackRouter.get('/:id', getFeedbackById);
+feedbackRouter.get('/:id', getFeedbackById);
 
 // Update feedback by ID
 feedbackRouter.put('/:id', updateFeedbackById);
 
 // Delete feedback by ID
-FeedbackRouter.delete('/:id', deleteFeedbackById);
+feedbackRouter.delete('/:id', deleteFeedbackById);
 
 export default feedbackRouter;
