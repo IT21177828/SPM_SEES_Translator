2083ab7b6cf9dd4762f0a94b00642f60365aaeb7
IT21177828 <it21177828@my.sliit.lk>
2023-09-10 01:56:25 +0530

    new changes to front end

--- backend/Routers/memberShipRouter.js
+++ backend/Routers/memberShipRouter.js
@@ -3,12 +3,12 @@ import mongoose from '../db/conn.js';
 
 // import {placeMembership} from '../controllers/memberShipController.js';
 import { activateMembership,deactivateMemberShip,reactivateMembership } from '../controllers/placeMembershipPlanController.js';
-
+import userController from '../controllers/userController.js';
 
 const membershipRouter = express.Router();
 
-membershipRouter.post('/',activateMembership);
-membershipRouter.post('/deactivate/:id',deactivateMemberShip);
-membershipRouter.post('/reactivate/:id',reactivateMembership);
+membershipRouter.post('/',userController.verify,activateMembership);
+membershipRouter.post('/deactivate/:id',userController.verify ,deactivateMemberShip);
+membershipRouter.post('/reactivate/:id', userController.verify,reactivateMembership);
 
 export default membershipRouter;
--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import userController from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 // import { registerUser,loginUser, checkAge, adminAccount } from '../controller/userController.js';
-import { registerUser,loginUser, checkAge } from '../controllers/userController.js';
+import { registerUser, checkAge } from '../controllers/userController.js';
 
 userRouter.post('/', registerUser);
-userRouter.get('/login', loginUser);
+userRouter.post('/login', userController.loginUser);
 userRouter.get('/checkAge', checkAge);
+
+
+userRouter.post('/refresh', userController.verify, userController.refresh);
+userRouter.post('/showName', userController.verify, userController.showName);
+
 // userRouter.post('/admin', adminAccount);
 
 export default userRouter;
